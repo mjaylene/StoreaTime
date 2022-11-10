77c74975bee3e8ae4faa9bb3391a7fb8312714db
mjaylene <mjaylene@stanford.edu>
2022-11-10 14:41:36 -0800

    Onboarding progress

--- StoreaTime-App/App.js
+++ StoreaTime-App/App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
+  const [loaded] = useFonts({
+    Romana: require('./assets/fonts/RomanaRoman-Normal.otf'),
+    'Romana-Bold': require('./assets/fonts/RomanaRoman-Bold.otf'),
+    //JakartaSans: require('./assets/fonts/PlusJakartaSans-VariableFont_wght'),
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.image}>
+        <Text style={styles.title}>StoreaTime</Text>
+        <StatusBar style="auto" />
+        <Pressable>
+          <View style={styles.signUpButton}>
+            <Text style={styles.signUpText}>Sign up</Text>
+          </View>
+        </Pressable>
+      </ImageBackground>
     </View>
   );
 }
@@ -17,4 +31,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    bottom: '5%',
+    color: 'white',
+    fontSize: 65,
+    fontFamily: 'Romana-Bold',
+  },
+  signUpButton: {
+    width: 350,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 12, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 280
+  },
+  signUpText: {
+    color: '#FF6F52',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 });
