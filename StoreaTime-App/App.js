f24b1d7204ea4323b4c514a35e744d8aad5cb8d1
mjaylene <mjaylene@stanford.edu>
2022-11-11 15:51:32 -0800

    First and second onboarding screens created

--- StoreaTime-App/App.js
+++ StoreaTime-App/App.js
@@ -1,28 +1,13 @@
 import { ImageBackground, StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import OnboardingScreens from './components/onboarding';
 
-
-export default function App() {
-  return (
-    <View style={styles.container}>
+/*
       <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.image}>
         <OnboardingScreens></OnboardingScreens>
       </ImageBackground>
-    </View>
+*/ 
+export default function App() {
+  return (
+      <OnboardingScreens></OnboardingScreens>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
--- StoreaTime-App/components/onboarding.js
+++ StoreaTime-App/components/onboarding.js
@@ -1,19 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image, SafeAreaView, ImageBackground } from 'react-native';
 import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {useNavigation} from '@react-navigation/native';
+import Signup from './Signup';
+import Login from './Login';
 
+const token = false
+//const Stack = createStackNavigator();
 
-export default function OnboardingScreens() {
-    const [loaded] = useFonts({
-        Romana: require('../assets/fonts/RomanaRoman-Normal.otf'),
-        'Romana-Bold': require('../assets/fonts/RomanaRoman-Bold.otf'),
-        //JakartaSans: require('./assets/fonts/PlusJakartaSans-VariableFont_wght'),
-      });
+//function HomeScreen({ navigation }) {
+
+//}
+const Stack = createStackNavigator();
+
+/*
+const SignupFunction = ( {navigation} ) => {
     return (
-        <View style={styles.onboarding}>
+
+    );
+}
+*/
+function HomeScreen({ navigation }) {
+    let contentDisplayed = null
+
+    if (token) {
+        console.log('returning user -> login')
+        // render flatlist
+        //contentDisplayed = <List trackList ={tracks} navigation={navigation}/>;
+    } else {
+        console.log('new user -> signup')
+        //contentDisplayed = <SignupFunction></SignupFunction>;
+    }
+
+    return (
+        <ImageBackground source={require('../assets/background.png')} resizeMode="cover" style={styles.image}>
             <Text style={styles.title}>StoreaTime</Text>
             <StatusBar style="auto" />
-            <Pressable>
+            <Pressable onPress={() => navigation.navigate('Signup')}>
                 <View style={styles.signUpButton}>
                     <Text style={styles.signUpText}>Sign up</Text>
                 </View>
@@ -24,49 +49,74 @@ export default function OnboardingScreens() {
                     </Pressable>
                 </View>
             </Pressable>
-        </View>
+        </ImageBackground>
+    );
+}
+
+// options={{ headerShown: false }}
+export default function OnboardingScreens() {
+    const [loaded] = useFonts({
+        Romana: require('../assets/fonts/RomanaRoman-Normal.otf'),
+        'Romana-Bold': require('../assets/fonts/RomanaRoman-Bold.otf'),
+        //JakartaSans: require('./assets/fonts/PlusJakartaSans-VariableFont_wght'),
+    });
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Back " component={HomeScreen} options={{headerShown: false}} />
+                <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}} /> 
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
 const styles = StyleSheet.create({
+    image: {
+        //flex: 1,
+        //aspectRatio: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+      },
     onboarding: {
         //backgroundColor: 'green',
         alignItems: 'center',
         width: '100%',
+        position: 'relative'
     },
     title: {
-      bottom: '5%',
-      color: 'white',
-      fontSize: 65,
-      fontFamily: 'Romana-Bold',
+        bottom: '5%',
+        color: 'white',
+        fontSize: 65,
+        fontFamily: 'Romana-Bold',
     },
     signUpButton: {
-      width: 350,
-      height: 50,
-      backgroundColor: 'white',
-      borderRadius: 12, 
-      justifyContent: 'center',
-      alignItems: 'center',
-      top: 280,
-      shadowColor: 'black',
-      shadowOffset: {width: 3, height: 6},
-      shadowOpacity: 0.3,
-      shadowRadius: 10,
+        width: 350,
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 280,
+        shadowColor: 'black',
+        shadowOffset: { width: 3, height: 6 },
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
     },
     signUpText: {
-      color: '#FF6F52',
-      fontSize: 16,
-      fontWeight: 'bold'
+        color: '#FF6F52',
+        fontSize: 16,
+        fontWeight: 'bold'
     },
     loginArea: {
-      top: 300,
-      flexDirection: 'row',
-      justifyContent: 'center'
+        top: 300,
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
     loginText: {
-      color: 'white',
-      textDecorationLine: 'underline',
-      fontWeight: 'bold',
+        color: 'white',
+        textDecorationLine: 'underline',
+        fontWeight: 'bold',
     }
-  });
-  
+});
